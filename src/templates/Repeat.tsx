bfcdfe49d814286dac1b33d5addb028ef772b065
Hamed Fathi <hamedfathi@outlook.com>
2021-07-05 19:44:10 +0200

    chore: Repeat component returns length

--- src/templates/Repeat.tsx
+++ src/templates/Repeat.tsx
@@ -2,32 +2,42 @@ import * as React from "react";
 import { ensureArray } from "utils";
 
 /*
-let i = 0;
-<Repeat for={[1, 2, 3, 4, 5]}>
-  {(value: any, index: any, array: any) => (
-    <>
-      <div style={{color:'red'}} key={i++}>This is item {value} in the list</div>
-      <Repeat for={[7, 8]}>
-        {(val: any, idx: any, arr: any) => (
-          <div style={{color:'blue'}} key={i++}>This is item {val} in the list</div>
+function App() {
+  let i = 0;
+  return (
+    <div className="App">
+      <Repeat for={[1, 2, 3, 4, 5]}>
+        {(value: any, index: any, array: any, len: any) => (
+          <>
+            <div key={i++}>{len}</div>
+            <div style={{ color: 'red' }} key={i++}>This is item {value} in the list</div>
+            <Repeat for={[7, 8]}>
+              {(v: any, inx: any, ar: any, l: any) => (
+                <>
+                  <div key={i++}>{l}</div>
+                  <div style={{ color: 'blue' }} key={i++}>This is item {v} in the list</div>
+                </>
+              )}
+            </Repeat>
+          </>
         )}
       </Repeat>
-    </>
-  )}
-</Repeat>
+    </div>
+  );
+}
 */
 
 // @ts-ignore
 export const Repeat = ({ for: items = [] as any[], children: render }) => {
-    items = ensureArray(items);
-    if (typeof render !== 'function') {
-        // @ts-ignore
-        render = (value: any) => null;
-    }
-    return (
-        <>
-            {items.map((value: any, index: any, array: any) => render(value, index, array))}
-        </>
-    );
+  items = ensureArray(items);
+  if (typeof render !== 'function') {
+    render = (_value: any) => null;
+  }
+  const length = items.length;
+  return (
+    <>
+      {items.map((value: any, index: any, array: any) => render(value, index, array, length))}
+    </>
+  );
 };
 
